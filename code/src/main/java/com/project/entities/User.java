20a50a9921e910580bc309f031f9782bb824c1d2
Tritryi <tristan.fontaniere@outlook.fr>
2026-05-05 20:06:34 +0200

    Fixing password leakage issue

--- code/src/main/java/com/project/entities/User.java
+++ code/src/main/java/com/project/entities/User.java
@@ -1,6 +1,7 @@
 package com.project.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,6 +25,7 @@ public class User {
     // to create an account
     private String username;
     private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     private String profilPicture;
